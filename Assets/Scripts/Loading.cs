887cf006712a289f861e49aec46e7da29e59f9af
internetcharles <charlesodellsmith@gmail.com>
2020-06-03 14:07:05 -0700

    added splash screen with adaptive sizing

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
 {
-    [SerializeField] AudioSource audioData = default;
-    private void Awake()
+
+    [SerializeField] private int waitTime = 3;
+
+    private int currentSceneIndex;
+
+    private void Start()
     {
-        audioData = GetComponent<AudioSource>();
-        audioData.Play(0);
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime(waitTime));
+        }
     }
+    IEnumerator WaitForTime(int waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
 
 }
