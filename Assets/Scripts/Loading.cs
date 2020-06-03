b6be4d20f8b505f1dcdaabcdb02b56b3fef5d852
internetcharles <charlesodellsmith@gmail.com>
2020-06-03 13:26:50 -0700

    splash screen, audio plays on start

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] AudioSource audioData = default;
+    private void Awake()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+    }
+
+}
