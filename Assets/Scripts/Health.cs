a21920dd9c25ef00b16ce149b23097ead752d84d
internetcharles <charlesodellsmith@gmail.com>
2020-06-07 22:53:34 -0700

    fixed all

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] int health = 100;
+    [SerializeField] private GameObject deathVFX = default;
+
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            TriggerDeathVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    public void TriggerDeathVFX()
+    {
+        if (!deathVFX)
+        {
+            return;
+        }
+        else
+        {
+            GameObject deathVFXObject = Instantiate(deathVFX, transform.position, Quaternion.identity);
+            Destroy(deathVFXObject, 1f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile = default;
+    [SerializeField] private GameObject gun = default;
+
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+
+}
